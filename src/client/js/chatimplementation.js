1b4251bfdec263c5431b8bb5759066e6c9c2f403
Sen Li <sen.li127@gmail.com>
2013-11-03 21:31:39 -0500

    working frontend

--- src/client/js/chatimplementation.js
+++ src/client/js/chatimplementation.js
@@ -1,40 +1,35 @@
 angular.module('Chatbox', ['ngResource']);
 
-function Chatbox($scope,$resource) {
-	$scope.userFound = true;
-	$scope.hasGallery = true;
-	$scope.displayGallery = false;
-	$scope.flickr = $resource('http://api.flickr.com/services/rest/',
-	{
-	api_key:'5c96da7d1ca991e1a1b05abfc4ad7fbb',
-	format:'json',
-	jsoncallback:'parseuser'},
-	{get:{method:'JSONP'}});
+
+
+function Chatbox($scope, $http, $resource) {
+	
+	$scope.method = 'POST';
+	$scope.url = 'localhost:1234';
 	
 	$scope.users = ["i.nadim", "mjval_lin", "jdery", "nick"];
 	
 	
-	send = function(from, to, message) {
-		
-		if(from == to || !isValidUser(to)) {
-			//alert
-		} else {
-			//post  w/o going to another page. 
-			//clear message box
-			//message sucessfully sent, get this from post result
-		}
-	}
+	$scope.postdata = function() {
+    $scope.code = null;
+    $scope.response = null;
+    alert(document.getElementById('s2').value);
+    var text  = document.getElementById('s2').value;
+    $http({method: $scope.method, url: $scope.url, cache: $templateCache, data : {varrrr: "post msg 1"}}).
+      success(function(data, status) {
+        $scope.status = status;
+        $scope.data = data;
+      }).
+      error(function(data, status) {
+        $scope.data = data || "Request failed";
+        $scope.status = status;
+    });
+  };
 	
-	isValidUser = function(username) {
-		for(var user in $scope.users) {
-			if(user = username) {
-				return true;
-			}		
-		}
-		
-		return false;
-	}
+  	$scope.updateModel = function(method, url) {
+    	$scope.method = method;
+    	$scope.url = url;
+  	};
 	
 	
 }
-//parseuser({"user":{"id":"12489505@N04", "nsid":"12489505@N04", "username":{"_content":"robzhu"}}, "stat":"ok"})
